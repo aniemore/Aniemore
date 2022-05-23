d1c86c69a59df6688ed4961c2193834d822b8d54
Ilya Lubenets <xfilinx@gmail.com>
2022-05-23 19:27:04 +0300

    now you can change yandex cloud folder id in your speech-2-text class

--- Aniemore/Tests/s2t_test.py
+++ Aniemore/Tests/s2t_test.py
@@ -2,7 +2,8 @@ from Aniemore.Utils.s2t import SpeechToText
 import os
 
 
-s2t_model = SpeechToText()
+s2t_model = SpeechToText(yandex_cloud_folder_id='b1g75n4nfd17pefq9el4')
+#s2t_model.set_yandex_cloud_folder_id()
 recognized_text = s2t_model.recognize(sound_data=open(f'{os.path.dirname(__file__)}/files/my_voice.ogg', 'rb').read())
 print(recognized_text['result'])
 echanced_text = s2t_model.echance_text(recognized_text)
--- Aniemore/Utils/s2t.py
+++ Aniemore/Utils/s2t.py
@@ -14,66 +14,81 @@ class SpeechToText:
     configs = {}
     grammar_model, apply_te = {}, {}
 
-    def __init__(self):
+    def __init__(self, yandex_cloud_folder_id: str):
         """
-        Init method for SpeechToText. Reads configs.yml on creation.
+        Init method for SpeechToText.
+        It takes a Yandex Cloud folder ID as an argument and saves it to the config file
+
+        :param yandex_cloud_folder_id: the ID of your folder in Yandex.Cloud
+        :type yandex_cloud_folder_id: str
         """
+
         try:
             with open('../config.yml', 'r') as config_file:
                 self.configs = yaml.safe_load(config_file)
         except yaml.YAMLError as ex:
             print(ex)
-        torch.backends.quantized.engine = 'qnnpack'  # Remove if you are not under Apple M1 on torch earlier version
+
+        self.configs['YandexCloud']['YC_FOLDER_ID'] = yandex_cloud_folder_id
+        try:
+            with open('../config.yml', 'w') as config_file:
+                yaml.safe_dump(self.configs, config_file)
+        except yaml.YAMLError as ex:
+            print(ex)
+        # Only Apple M1 fixes, comment if under other OS
+        # torch.backends.quantized.engine = 'qnnpack'
         self.grammar_model, _, _, _, self.apply_te = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                                                     model='silero_te')
 
     def recognize(self, sound_data: bytes) -> dict:
         """
-        :param sound_data: .ogg file, less than 30s, only 48kHz, only 16 kbps
-        :param ID_FOLDER: your yandex cloud folder id
-        :return: string of recognized text
-        """
+        We send a POST request to the Yandex Cloud server with the audio file, and get a JSON response with the text
 
-        # в поле заголовка передаем YC_IAM_TOKEN:
+        :param sound_data: binary audio (.ogg file, less than 30s, only 48kHz, only 16 kbps)
+        :type sound_data: bytes
+        :return: The text of the audio file.
+        """
         headers = {'Authorization': f'Bearer {self.__get_iam_token()}'}
 
-        # остальные параметры:
         params = {
             'lang': 'ru-RU',
             'folderId': self.configs['YandexCloud']['YC_FOLDER_ID'],
             'sampleRateHertz': 48000,
         }
 
-        # Делаем запрос на сервера SpeechKit
         response = requests.post(self.configs['YandexCloud']['YC_URL'], params=params, headers=headers, data=sound_data)
-
-        # бинарные ответ доступен через response.content, декодируем его:
         decode_resp = response.content.decode('UTF-8')
-
-        # и загрузим в json, чтобы получить текст из аудио:
         text = json.loads(decode_resp)
 
         if text.get('result') is None:
-            raise Exception("YancexCloud response error. Answer does not ccontain 'result' field")
+            raise Exception(text)
 
         return text
 
     def echance_text(self, text: str) -> str:
         """
-        Enchances given text with commas, question marks, etc.
-        :return: string
+        It takes a string, applies a grammar to it, and returns the result
+
+        :param text: The text to be corrected
+        :type text: str
+        :return: The text is being returned.
         """
         text = text.get("result", "").lower()
         grammar_text = self.apply_te(text, lan="ru")
         return grammar_text
 
     def __get_iam_token(self):
-
+        """
+        If the token is not set or it's older than 12 hours, create a new one and save it to the config file
+        :return: The token is being returned.
+        """
         if (
                 self.configs['YandexCloud']['YC_IAM_TOKEN']['token'] is None or
+                self.configs['YandexCloud']['YC_IAM_TOKEN']['token'] == '' or
                 self.configs['YandexCloud']['YC_IAM_TOKEN']['created_at'] is None or
                 abs(self.configs['YandexCloud']['YC_IAM_TOKEN']['created_at'] - time()) >= 3600 * 12
         ):
+            # Make shure, that YandexCloud-cli is installed, added to PATH and configured properly
             self.configs['YandexCloud']['YC_IAM_TOKEN']['token'] = popen("yc iam create-token").read().replace("\n", "")
             self.configs['YandexCloud']['YC_IAM_TOKEN']['created_at'] = time()
             try:
