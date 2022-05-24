0268ac57a3ed3700eaff509ed9855408e9cf0757
= <=>
2022-05-24 04:46:30 +0300

    added requires from requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+from os import path
+
+requirements_path = path.join(path.dirname(__file__), "requirements.txt")
+
+with open(requirements_path, "r") as f:
+    requirements = [x for x in f.read().split("\n") if x]
 
 setup(
     name='Aniemore',
-    version='0.0.0.1',
+    install_requires=requirements,
+    version='0.1.0',
     packages=['Aniemore', 'Aniemore.Text', 'Aniemore.Tests', 'Aniemore.Utils', 'Aniemore.Voice'],
     url='https://github.com/aniemore/Aniemore',
     license='GPL-3.0 license ',
